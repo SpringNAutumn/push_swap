6754438cef4ef130bf4e08f5f4acaa43445d42c3
gmarinm <guillecrew@hotmail.com>
2024-07-15 20:59:43 +0200

    competando

--- README.md
+++ README.md
@@ -1,18 +1,9 @@
 ## pasos a seguir ##
 
 
-* orenacion por comparacion utilizando push y swaps.
+- Calculo de coste 
+- Movimiento de nodo
+- Funcion 
+- calcular el ahorro de la mitad mas adelante
 
-Algoritmo eficiente ->
-Calcular antes de realizar operaciones
-calcular costes, contaremos y calcularemos antes de mover a A,
-Si movemos un maximo, tiene que posicionarse por encima del maximo del stackA,
-las operaciones se calcularan en base a cuantos movimientos debemos de llevar a cabo hasta mover el elemento, al maximo o minimo o maximo. 
-
-	Pushear todos los elementos al stack B y comprobar si quedan ordenados. 
-    	Comprobar mecanismos del menor coste. 
-	Encontrar maximo y moverlo al principio, pushear a A.
-    
--- Aplicar otros alogritmos mas eficientes, algoritmo recursivo?
--- Aplicar tester propio para ver si los costes sobrepasan los baremos de corrección.
 
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gmarin-m <gmarin-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 18:59:23 by gmarin-m          #+#    #+#             */
-/*   Updated: 2024/07/15 18:47:20 by gmarin-m         ###   ########.fr       */
+/*   Updated: 2024/07/15 20:58:37 by gmarin-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main (int argc, char *argv[])
 	t_stack *stack_A;
 	t_stack *stack_B;
 
-	char *arrelenar[] = {"0", "10", "5", "4", "1" , NULL};
+	char *arrelenar[] = {"0", "10", "6", "1" , NULL};
 
 	stack_A = NULL;
 	stack_B = NULL;
@@ -28,8 +28,8 @@ int main (int argc, char *argv[])
 	bigAlgo(&stack_A, &stack_B);
 
 	printf("resultado de mover en stackB el cheaper node \n");
-	
 	ft_lstiter(stack_B,printing);
+	
     return (0);
 }
 
@@ -132,7 +132,7 @@ void goodSort (t_stack **stackA, t_stack **stackB)
 			{
 				// para el elemento calculamos coste guardamos el resultado,
 				// vemos cual es el menor resultado,
-						// ejecutamos operacion ->>> 
+				// ejecutamos operacion ->>> 
 			}
 	}
 }
@@ -197,112 +197,86 @@ int getMin (t_stack **stack)
 	return min;
 }
 
-
-// coste de mover el nodo especificado, (el 1) en el stack b, esto es poner el inmediatamente menor arriba del todo.
+// coste de mover el nodo especificado en el stack B.
 int calculateCostB (t_stack **stack, t_stack *node)
 {
 	int cost;
-	printf("coste de mover el nodo especificado, (el 1) en el stack b %d ", get_pos(stack, getRightPos(node -> content, stack)));
-	cost = 0;
-	if (get_pos(stack, getRightPos(node -> content, stack)) <= ((ft_lstsize(*stack) / 2)))
-		cost = get_pos(stack, getRightPos(node -> content, stack)) - 1;
-	else
-		cost = ft_lstsize(*stack) - get_pos(stack, getRightPos(node -> content, stack)) + 1;
+	cost = get_pos(stack, getRightPos(node -> content, stack)) - 1;
 	return cost;
 }
 
+// Total coste de movimientos, para colocar en posicion idonea.
 int calculateCostA (t_stack **stack, t_stack *node)
 {
 	int cost;
-
-	cost = 0;
-	if(get_pos(stack, node) <= (ft_lstsize(*stack)) / 2)
-		cost = get_pos(stack,node) - 1;
-	else
-		cost = ft_lstsize(*stack) - get_pos(stack,node) + 1;
+	cost = get_pos(stack,node);
 	return cost;
 }
 
 void moveBcheaperNode (t_stack **stackB, t_stack *node)
 {
-	printf("valor del nodo: %d \n" , node -> content);
-	printf("posicion del nodo que tenemos que mover : %d ", get_pos(stackB,getRightPos(node ->content, stackB)));
-	if (get_pos(stackB,getRightPos(node ->content, stackB)) < (ft_lstsize(*stackB) / 2))
-		while(*stackB != getRightPos(node -> content, stackB))
-			printf("iprimiendo: %s", rotate_b(stackB));
-	else if (get_pos(stackB,getRightPos(node ->content, stackB)) > (ft_lstsize(*stackB) / 2))
-		while (*stackB != getRightPos(node -> content, stackB))
-			printf(" impriminendo: %s", reverse_rotate_b(stackB));
+	while(*stackB != getRightPos(node -> content, stackB))
+		printf("iprimiendo: %s", rotate_b(stackB));
 }
 
 void moveinAandToB (t_stack **stackA, t_stack **stackB, t_stack *node)
 {
-
-printf("valor del nodo: %d" , node -> content);
-	if (get_pos(stackA, node) < (ft_lstsize(*stackA) / 2))
-		while(*stackA != node)
-			rotate_a(stackA);
-	else if (get_pos(stackA, node) > (ft_lstsize(*stackA) / 2))
-		while (*stackA != node)
-			reverse_rotate_a(stackA);
+	printf("PONGO ==> valor cheap node : %d puntero %p \n", node ->content ,node );
+	printf("valor stack %p\n", *stackA);
+	while(*stackA != node)
+	{
+		rotate_a(stackA);
+		//printf("valor cheap node : %d puntero %p", node ->content ,node);
+	}
 	if (*stackA == node)
-		push_b(stackA, stackB);
+		printf("%s",push_b(stackA, stackB));
 }
 
 void bigAlgo(t_stack **stackA, t_stack **stackB)
 {
 	t_stack *aux;
 	
-	
 	int cost = 0;
 	push_b(stackA,stackB);
 	push_b(stackA,stackB);
+	
 	aux = *stackA;
 	ft_lstiter(*stackB,printing);
 	int currentMinCost = __INT_MAX__;
 	t_stack *cheaperNode;
 
 	cheaperNode = *stackA;
-	int numero = 3;
-	while (numero > 0)
+	while (*stackA)
 	{
 		while (aux)
 		{
 			cost = calculateCostA(stackA, aux);
 			cost += calculateCostB(stackB, aux);
-			
-		
-			printf("coste: %d, valor variable auxiliar: %d", cost , aux -> content);
 			if (cost < currentMinCost)
 			{
 				currentMinCost = cost;
 				cheaperNode = aux;
-				printf("nodo mas barato: %d" , cheaperNode -> content);
 			}
 			aux = aux -> next;
 		}
-		printf("nodo mas barato: %d" , cheaperNode -> content);
-		//printf ("valor del cheaper node: %d , puntero del cheaper node: %p", cheaperNode -> content , cheaperNode);
-		moveBcheaperNode (stackB, cheaperNode);
-		//moveinAandToB(stackA,stackB,cheaperNode);
-
-		numero --;	
+		//moveBcheaperNode(stackB, cheaperNode);
+		printf("PINGO ==> valor cheap node : %d puntero %p\n", cheaperNode ->content ,cheaperNode);
+		moveinAandToB(stackA,stackB,cheaperNode);
 	}
 }
 
 /*
-		  5
-		  4
-		  1
+		  20
+		  16
 		  10
-          
-		  
-	
-	2
-	10
-	2
-	9
-	3
+		  6
+		  1
+_____________________________
+
+	14   
+	2	 
+	9     
+	3     
 	8
    21
     7
